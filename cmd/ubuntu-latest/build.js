db9ecfceb5e46163335f2f340b657b5f1f39769a
Jonas Franke <jonasfranke@sdevs.org>
2024-04-28 20:56:08 +0200

    fixes

--- cmd/ubuntu-latest/build.js
+++ cmd/ubuntu-latest/build.js
@@ -82,6 +82,7 @@ const downloadRepoFuncMinors = async (
   tree_sha,
   folderPath = browserFolder,
   minor,
+  isRecursiveCall = false,
 ) => {
   try {
     const {
@@ -95,17 +96,22 @@ const downloadRepoFuncMinors = async (
       },
     );
 
-    const minorPath = path.resolve(folderPath, "packages", minor);
+    const minorPath = isRecursiveCall
+      ? folderPath
+      : path.resolve(folderPath, "packages", minor);
 
     if (!fs.existsSync(minorPath)) {
       fs.mkdirSync(minorPath, { recursive: true });
     }
 
     for (const item of tree) {
-      const filePath = path.resolve(minorPath, item.path);
+      let filePath = path.resolve(minorPath, item.path);
 
       if (item.type === "tree") {
-        fs.mkdirSync(filePath, { recursive: true });
+        if (!fs.existsSync(filePath)) {
+          fs.mkdirSync(filePath, { recursive: true });
+        }
+        await downloadRepoFuncMinors(item.sha, filePath, minor, true);
       } else if (item.type === "blob") {
         try {
           const {
@@ -140,7 +146,12 @@ const downloadRepoFuncMinors = async (
       const waitTime = resetTime - Math.floor(Date.now() / 1000);
       console.log(`Rate limit exceeded. Waiting for ${waitTime} seconds...`);
       await delay(waitTime * 1000);
-      return downloadRepo(tree_sha, folderPath);
+      return downloadRepoFuncMinors(
+        tree_sha,
+        folderPath,
+        minor,
+        isRecursiveCall,
+      );
     } else {
       throw error;
     }
@@ -157,6 +168,7 @@ const downloadMinorRepos = async (folderPath = browserFolder) => {
     }
 
     for (const minor of minors) {
+      console.log(`Downloading the ${minor} repo...`);
       const {
         data: {
           object: { sha },
@@ -167,74 +179,35 @@ const downloadMinorRepos = async (folderPath = browserFolder) => {
         ref: "heads/main",
       });
 
-      const {
-        data: { tree },
-      } = await octokit.request(
-        "GET /repos/{owner}/{repo}/git/trees/{tree_sha}",
-        {
-          owner: "PraxiveSoftware",
-          repo: minor,
-          tree_sha: sha,
-        },
-      );
-
-      const minorPath = path.resolve(minorsPath, minor);
-
-      if (!fs.existsSync(minorPath)) {
-        fs.mkdirSync(minorPath, { recursive: true });
-      }
-
-      for (const item of tree) {
-        const filePath = path.resolve(minorPath, item.path);
-
-        if (item.type === "tree") {
-          fs.mkdirSync(filePath, { recursive: true });
-          await downloadRepoFuncMinors(item.sha, folderPath, minor);
-        } else if (item.type === "blob") {
-          try {
-            const {
-              data: { content },
-            } = await octokit.request(
-              "GET /repos/{owner}/{repo}/git/blobs/{file_sha}",
-              {
-                owner: "PraxiveSoftware",
-                repo: minor,
-                file_sha: item.sha,
-              },
-            );
-
-            fs.writeFileSync(filePath, Buffer.from(content, "base64"));
-          } catch (error) {
-            if (error.status === 404) {
-              console.warn(
-                `Warning: File ${item.path} is a directory. Already downloaded, skipping...`,
-              );
-            } else {
-              throw error;
-            }
-          }
-        }
-      }
+      await downloadRepoFuncMinors(sha, folderPath, minor);
+      console.log(`Downloaded the ${minor} repo.`);
 
       if (minor !== "pdf-viewer") {
         console.log(`Installing dependencies for ${minor}...`);
-        exec("yarn", { cwd: minorPath }, (error) => {
-          if (error) {
-            console.error(
-              `Error during dependency installation for ${minor}: ${error}`,
-            );
-            throw error;
-          } else {
-            console.log(`Building ${minor}...`);
-            exec("yarn build", { cwd: minorPath }, (error) => {
-              if (error) {
-                console.error(`Error during build for ${minor}: ${error}`);
-                throw error;
-              } else {
-                console.log(`Built ${minor}.`);
-              }
-            });
-          }
+        await new Promise((resolve, reject) => {
+          exec("yarn", { cwd: path.resolve(minorsPath, minor) }, (error) => {
+            if (error) {
+              console.error(
+                `Error during dependency installation for ${minor}: ${error}`,
+              );
+              reject(error);
+            } else {
+              console.log(`Building ${minor}...`);
+              exec(
+                "yarn build",
+                { cwd: path.resolve(minorsPath, minor) },
+                (error) => {
+                  if (error) {
+                    console.error(`Error during build for ${minor}: ${error}`);
+                    reject(error);
+                  } else {
+                    console.log(`Built ${minor}.`);
+                    resolve();
+                  }
+                },
+              );
+            }
+          });
         });
       }
     }
@@ -247,7 +220,7 @@ const downloadMinorRepos = async (folderPath = browserFolder) => {
       const waitTime = resetTime - Math.floor(Date.now() / 1000);
       console.log(`Rate limit exceeded. Waiting for ${waitTime} seconds...`);
       await delay(waitTime * 1000);
-      return downloadRepo(tree_sha, folderPath);
+      return downloadMinorRepos(folderPath);
     } else {
       throw error;
     }
@@ -256,27 +229,19 @@ const downloadMinorRepos = async (folderPath = browserFolder) => {
 
 const buildBrowser = () => {
   return new Promise((resolve, reject) => {
-    console.log("Checking if yarn is installed...");
-    exec("yarn --version", (error) => {
+    console.log("Installing dependencies...");
+    exec("yarn", { cwd: browserFolder }, (error) => {
       if (error) {
-        console.error(`Yarn is not installed: ${error}`);
+        console.error(`Error during dependency installation: ${error}`);
         reject(error);
       } else {
-        console.log("Installing dependencies...");
-        exec("yarn", { cwd: browserFolder }, (error) => {
+        console.log("Building the browser...");
+        exec("yarn build", { cwd: browserFolder }, (error) => {
           if (error) {
-            console.error(`Error during dependency installation: ${error}`);
+            console.error(`Error during build: ${error}`);
             reject(error);
           } else {
-            console.log("Building the browser...");
-            exec("yarn build", { cwd: browserFolder }, (error) => {
-              if (error) {
-                console.error(`Error during build: ${error}`);
-                reject(error);
-              } else {
-                resolve();
-              }
-            });
+            resolve();
           }
         });
       }
@@ -286,7 +251,7 @@ const buildBrowser = () => {
 
 const createInstallers = async () => {
   return new Promise((resolve, reject) => {
-    exec(`yarn compile-linux`, { cwd: browserFolder }, async (error) => {
+    exec("yarn compile-linux", { cwd: browserFolder }, async (error) => {
       if (error) {
         console.error(`Error during compile: ${error}`);
         reject(error);
@@ -302,7 +267,7 @@ const createInstallers = async () => {
           fs.mkdirSync(versionFolder, { recursive: true });
         }
 
-        const nsisWebFolder = path.join(browserFolder, "dist");
+        const nsisWebFolder = path.join(browserFolder, "dist", "nsis-web");
         if (!fs.existsSync(nsisWebFolder)) {
           console.error(
             `Error: ${nsisWebFolder} does not exist. The compile step may have failed or it may not have created this directory as expected.`,
@@ -330,8 +295,8 @@ const createRelease = async () => {
   } = await octokit.request("POST /repos/{owner}/{repo}/releases", {
     owner: "PraxiveSoftware",
     repo: "releases",
-    tag_name: "v" + currentVersion,
-    name: "v" + currentVersion,
+    tag_name: currentVersion,
+    name: currentVersion,
     prerelease: true,
   });
 
@@ -411,7 +376,7 @@ const main = async () => {
       const waitTime = resetTime - Math.floor(Date.now() / 1000);
       console.log(`Rate limit exceeded. Waiting for ${waitTime} seconds...`);
       await delay(waitTime * 1000);
-      return downloadRepo(tree_sha, folderPath);
+      return main();
     } else {
       throw error;
     }
